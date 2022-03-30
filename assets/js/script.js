af45ec31c09000b8646a0b2e3356b77230b72434
Eric Webber <ewebber283@gmail.com>
2022-03-30 18:17:32 -0500

    added form submit handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,28 @@ const cityFormEl = document.querySelector('#city-form')
 const cityInputEl = document.querySelector('#city')
 const cityContainerEl = document.querySelector('#cities-container')
 const citySearchTerm = document.querySelector('#city-search-term')
+
+ /*getCities = (city) => {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.value + '&appid=61f172bd2a4837729bfd40bf2e1005cb&units=imperial'
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+} */
+
+formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const city = cityInputEl.value.trim();
+
+    if(city) {
+        getCities(city)
+        cityInputEl.value = '';
+    } else {
+        alert('Please Enter the Name of a City!')
+    }
+}
+
+
+cityFormEl.addEventListener('click', formSubmitHandler);

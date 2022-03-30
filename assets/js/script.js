157096240983d157917881bf62915bda0bdbfe2b
Eric Webber <ewebber283@gmail.com>
2022-03-30 17:55:02 -0500

    finished assigning html classes

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const cityFormEl = document.querySelector('#city-form')
+const cityInputEl = document.querySelector('#city')
+const cityContainerEl = document.querySelector('#cities-container')
+const citySearchTerm = document.querySelector('#city-search-term')

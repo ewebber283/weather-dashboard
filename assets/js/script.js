ddbee864d12fd5ec067e383cbdcce75cb6ee1354
Eric Webber <ewebber283@gmail.com>
2022-04-01 16:05:31 -0500

    form submit working for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,15 @@ const cityInputEl = document.querySelector('#city')
 const cityContainerEl = document.querySelector('#cities-container')
 const citySearchTerm = document.querySelector('#city-search-term')
 
- /*getCities = (city) => {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.value + '&appid=61f172bd2a4837729bfd40bf2e1005cb&units=imperial'
+ getCities = (city) => {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=61f172bd2a4837729bfd40bf2e1005cb&units=imperial'
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         })
     })
-} */
+} 
+
 
 formSubmitHandler = (event) => {
     event.preventDefault();
@@ -26,4 +27,4 @@ formSubmitHandler = (event) => {
 }
 
 
-cityFormEl.addEventListener('click', formSubmitHandler);
+cityFormEl.addEventListener('click', formSubmitHandler); 
